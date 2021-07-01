b549f032d48a30646a7d78941ab942e2667cf801
tjones210 <48491008+tjones210@users.noreply.github.com>
2021-07-01 14:26:36 -0600

    Add files via upload

--- md_score.py
+++ md_score.py
@@ -1,4 +1,7 @@
 ######################################### Imports #########################################
+import subprocess
+from functools import partial
+import multiprocessing
 import pandas as pd
 import math
 import numpy as np
@@ -11,10 +14,12 @@ from os import path
 import warnings
 from pandas.core.common import SettingWithCopyWarning
 warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
+
 ######################################### MD Score Main #########################################
+
 def run_md_score(verbose, outdir, sample, window, cpus):
     if verbose == True: 
-        print('--------------Beginning MD-Score Calculation---------------')
+        print('--------------Beginning MD-Score Calculation- Simulated---------------')
         print('Initializing ' + str(cpus) + ' threads to calculate MD Scores.')
         start_time = int(time.time())
         print('Start time: %s' % str(datetime.datetime.now()))
@@ -23,30 +28,82 @@ def run_md_score(verbose, outdir, sample, window, cpus):
     if verbose==True:
         print('--------------Normalizing Motif Quality Scores---------------')
     ##func to read in each motif file present in a dir
-    motif_file = outdir + '/motifs/simulated/TP53_M06704_1.sorted.bed'
+    ###NEED TO LOOP THROUGH
+    tf= 'TP53_M06704_1'
+    motif_file = outdir + '/motifs/simulated/' + tf + '.sorted.bed'
     motif_df = calculate_motif_quality_score(motif_file=motif_file)
+    annotation_df = read_annotation(verbose=verbose, sample=sample, 
+                                    outdir=outdir, seq_type='simulated')
     
     if verbose == True:
-        print("---------Calculating Motif Distance Scores----------")
-    annotation_file = outdir + '/annotations/' + sample + '_simulated_window.bed'
-    motif_score_df = get_distances(verbose=verbose, motif_df=motif_df, 
-                                  annotation_file=annotation_file)
+        print("---------Pulling Chromosomes From Motif and Annotation Files----------") 
+    chrs = get_chrs(verbose=verbose, motif_df=motif_df, annotation_df=annotation_df)
+    if verbose == True:
+        print("---------Calculating Motif Distance Scores----------")  
+    pool = multiprocessing.Pool(cpus)
+    motif_distance_dfs = pool.map(partial(get_distances, 
+                           inputs=[verbose, motif_df, annotation_df]), chrs)
+    motif_distance_df = pd.concat(motif_distance_dfs, axis=0)
+    
     motif_score_df = calculate_motif_distance_score(verbose=verbose, outdir=outdir, sample=sample,
-                                                    motif_score_df=motif_score_df, 
-                                                    window=window, distance_weight=0.1,seq_type='simulated')
+                                                    motif_distance_df=motif_distance_df, 
+                                                    window=window, distance_weight=0.1,tf=tf, seq_type='simulated')
     
     if verbose == True:
         print("---------Calculating Region Score----------")
     region_score_df = calculate_region_score(verbose=verbose, outdir=outdir, sample=sample, 
-                                             motif_score_df=motif_score_df,seq_type='simulated')
+                                             motif_score_df=motif_score_df, tf=tf, seq_type='simulated')
     
     if verbose == True:
         print("---------MD-Score Calculation Complete----------")
         stop_time = int(time.time())
         print('Stop time: %s' % str(datetime.datetime.now()))
         print('Total Run time :', (stop_time-start_time)/60, ' minutes')
-
-
+        
+###################################################################################################        
+    if verbose == True: 
+        print('--------------Beginning MD-Score Calculation- Experimental---------------')
+        print('Initializing ' + str(cpus) + ' threads to calculate MD Scores.')
+        start_time = int(time.time())
+        print('Start time: %s' % str(datetime.datetime.now()))
+    md_dirs(verbose, outdir=outdir, seq_type='experimental')
+    
+    if verbose==True:
+        print('--------------Normalizing Motif Quality Scores---------------')
+    ##func to read in each motif file present in a dir
+    ###NEED TO LOOP THROUGH
+    tf= 'TP53_M06704_1'
+    motif_file = outdir + '/motifs/experimental/' + tf + '.sorted.bed'
+    motif_df = calculate_motif_quality_score(motif_file=motif_file)
+    annotation_df = read_annotation(verbose=verbose, sample=sample, 
+                                    outdir=outdir, seq_type='experimental')
+    
+    if verbose == True:
+        print("---------Pulling Chromosomes From Motif and Annotation Files----------") 
+    chrs = get_chrs(verbose=verbose, motif_df=motif_df, annotation_df=annotation_df)
+    if verbose == True:
+        print("---------Calculating Motif Distance Scores----------")  
+    pool = multiprocessing.Pool(cpus)
+    motif_distance_dfs = pool.map(partial(get_distances, 
+                           inputs=[verbose, motif_df, annotation_df]), chrs)
+    motif_distance_df = pd.concat(motif_distance_dfs, axis=0)
+    
+    motif_score_df = calculate_motif_distance_score(verbose=verbose, outdir=outdir, sample=sample,
+                                                    motif_distance_df=motif_distance_df, 
+                                                    window=window, distance_weight=0.1,tf=tf, seq_type='experimental')
+    
+    if verbose == True:
+        print("---------Calculating Region Score----------")
+    region_score_df = calculate_region_score(verbose=verbose, outdir=outdir, sample=sample, 
+                                             motif_score_df=motif_score_df, tf=tf, seq_type='experimental')
+    
+    if verbose == True:
+        print("---------MD-Score Calculation Complete----------")
+        stop_time = int(time.time())
+        print('Stop time: %s' % str(datetime.datetime.now()))
+        print('Total Run time :', (stop_time-start_time)/60, ' minutes')
+        
+        
 ######################################### MD Score Functions #########################################
 def md_dirs(verbose, outdir, seq_type):
     if (path.exists(outdir + '/temp') == False):
@@ -59,15 +116,7 @@ def md_dirs(verbose, outdir, seq_type):
     else:
         if verbose == True:
             print('Successfully created the directory %s' % outdir + '/temp/' + seq_type + '_motif_scores')
-            
-    try:
-        os.system('mkdir -p ' + outdir + '/temp/' + seq_type + '_region_scores')
-    except OSError:
-        print ('Creation of the directory %s failed' % outdir + '/temp/' + seq_type + '_region_scores')
-        sys.exit(1)
-    else:
-        if verbose == True:
-            print('Successfully created the directory %s' % outdir + '/temp/' + seq_type + '_region_scores')
+
 
 def calculate_motif_quality_score(motif_file):
     motif_df = pd.read_csv(motif_file, sep='\t', header=None)
@@ -77,21 +126,25 @@ def calculate_motif_quality_score(motif_file):
     motif_df['normalized_quality_score'] = motif_df['score']/((motif_df['score']).max())
     return motif_df
 
-def get_distances(verbose, motif_df, annotation_file):
+def read_annotation(verbose, sample, outdir, seq_type):
     if verbose == True:
         print('Reading annotation file and centering regions...')
+    annotation_file = outdir + '/annotations/' + sample + '_' + seq_type + '_window.bed'
     annotation_df = pd.read_csv(annotation_file, sep='\t', header=None)
     annotation_df.columns = ['chr', 'start', 'stop', 'region_name']
     annotation_df['center'] = round((annotation_df['stop'] + annotation_df['start'])/2)
-    
+    return annotation_df
+
+
+def get_chrs(verbose, motif_df, annotation_df):
     if verbose == True:
         print('Getting chromosome list...')
     all_chr_motif = list(motif_df['chr'].unique())
     all_chr_annotation = list(annotation_df['chr'].unique())
-    all_chrs = list(set(all_chr_motif + all_chr_annotation))
+    chrs = list(set(all_chr_motif + all_chr_annotation))
     if verbose == True:
-        print('There are ' + str(len(all_chrs)) + ' "chromosomes" in this dataset. They are:')
-        print(all_chrs)
+        print('There are ' + str(len(chrs)) + ' "chromosomes" in this dataset. They are:')
+        print(chrs)
         #checking for odd ball chromosomes
         mtf = set(all_chr_motif)
         ann = set(all_chr_annotation)
@@ -103,25 +156,8 @@ def get_distances(verbose, motif_df, annotation_file):
             print(annotation_not_motif + ' chromosomes are unique to the annotation file')
         else:
             print('The chromosomes match between the motif and annotation files.')
-        print('Calculating distances...')
+    return chrs
 
-    distances =[]
-    for chrs in all_chrs:
-        if verbose == True:
-            print('Calculating distances for chromosome ' + chrs + '.')
-        single_chr_motif = motif_df[motif_df['chr'] == chrs]
-        single_chr_annotation = annotation_df[annotation_df['chr'] == chrs]
-
-        single_chr_motif['dis_rn'] = single_chr_motif.apply(lambda row: findbedregions(row, single_chr_annotation), axis=1)
-        single_chr_motif_distances = single_chr_motif[['dis_rn']]
-        distances.append(single_chr_motif_distances)
-
-    distance_df = pd.concat(distances, ignore_index=False)
-    motif_score_df = motif_df.merge(distance_df, left_index=True, right_index=True)
-    motif_score_df[['distance','region_name']] = pd.DataFrame(motif_score_df.dis_rn.tolist(), index= motif_score_df.index)
-    motif_score_df = motif_score_df.drop(['dis_rn'], axis=1)
-    return motif_score_df
-    
 def findbedregions(row, single_chr_annotation):
     center = row['center']
     hits_df = single_chr_annotation[single_chr_annotation['start'] < center]
@@ -130,18 +166,48 @@ def findbedregions(row, single_chr_annotation):
     region_name = list(hits_df['region_name'])
     return [distance[0], region_name[0]]
 
+def get_distances(chrs, inputs):
+    verbose, motif_df, annotation_df = inputs
+    if verbose == True:
+        print('Calculating distances for chromosome ' + chrs + '.') 
+        
+    distances =[]
+    single_chr_motif = motif_df[motif_df['chr'] == chrs]
+    if verbose == True:
+        before_drop = (len(single_chr_motif))
+    single_chr_motif = single_chr_motif.sort_values(by='normalized_quality_score', ascending=False)
+    single_chr_motif = single_chr_motif.drop_duplicates(subset=['center'], keep='first')
+    if verbose == True:
+        after_drop = (len(single_chr_motif))
+        dropped = before_drop - after_drop
+        if dropped != 0:
+            print(str(dropped) + ' hits were dropped on chromosome ' + chrs + ' due to duplicate hits.\n')  
+        else:
+            print('No hits were dropped on chromosome ' + chrs + '.\n') 
+    single_chr_annotation = annotation_df[annotation_df['chr'] == chrs]
+
+    single_chr_motif['dis_rn'] = single_chr_motif.apply(lambda row: findbedregions(row, single_chr_annotation), axis=1)
+    single_chr_motif_distances = single_chr_motif[['dis_rn']]
+    distances.append(single_chr_motif_distances)
+
+    distance_df = pd.concat(distances, ignore_index=False)
+    motif_distance_df = motif_df.merge(distance_df, left_index=True, right_index=True)
+    motif_distance_df[['distance','region_name']] = pd.DataFrame(motif_distance_df.dis_rn.tolist(), index= motif_distance_df.index)
+    motif_distance_df = motif_distance_df.drop(['dis_rn'], axis=1)
+    return motif_distance_df
+
 def no0s(row):
     if (row['distance'] == 0):
         return row['distance'] + 0.99999
     else:
         return row['distance']
 
-def calculate_motif_distance_score(verbose, outdir, sample, motif_score_df, window, distance_weight, seq_type):
-    rhf = motif_score_df.groupby('region_name').count().reset_index()
+def calculate_motif_distance_score(verbose, outdir, sample, motif_distance_df, window, distance_weight, tf, seq_type):
+    rhf = motif_distance_df.groupby('region_name').count().reset_index()
     rhf = rhf[['region_name', 'chr']]
     rhf.columns = ['region_name', 'region_hit_frequency']
-    motif_score_df=motif_score_df.merge(rhf, on='region_name')
-    motif_score_df['distance'] = motif_score_df.apply(lambda row: no0s(row), axis=1)
+    motif_distance_df=motif_distance_df.merge(rhf, on='region_name')
+    motif_distance_df['distance'] = motif_distance_df.apply(lambda row: no0s(row), axis=1)
 
     ##Calculating distance scores
     #this sets the weight of the distance score
@@ -150,14 +216,15 @@ def calculate_motif_distance_score(verbose, outdir, sample, motif_score_df, wind
     #where small_window is 10% of the size of the large window
     small_window = window*0.1
     exponent = math.log(distance_weight, small_window)
+    motif_score_df = motif_distance_df
     motif_score_df['normalized_distance_score'] = (abs(motif_score_df['distance']))**exponent
-    motif_score_df['motif_hit_score'] = (0.2*motif_score_df['normalized_quality_score'] + 0.8*motif_score_df['normalized_distance_score'])*(1/motif_score_df['region_hit_frequency'])
-    motif_score_df.to_csv(outdir + '/temp/' + seq_type + '_motif_scores/' + sample + '_tp53.txt', sep='\t', index=False)
+    motif_score_df['motif_hit_score'] = (0.2*motif_score_df['normalized_quality_score'] + 0.8*motif_score_df['normalized_distance_score'])
+    motif_score_df.to_csv(outdir + '/temp/' + seq_type + '_motif_scores/' + sample + '_' + tf + '.txt', sep='\t', index=False)
     return motif_score_df
 
-def calculate_region_score(verbose, outdir, sample, motif_score_df, seq_type): 
+def calculate_region_score(verbose, outdir, sample, motif_score_df, tf, seq_type): 
     region_score_df = motif_score_df[['region_name', 'motif_hit_score']]
     region_score_df = region_score_df.groupby('region_name').sum().reset_index()
     region_score_df.columns = ['region_name', 'region_score']
-    region_score_df.to_csv(outdir + '/temp/' + seq_type + '_region_scores/' + sample +'_tp53.txt', sep='\t', index=False)
+    region_score_df.to_csv(outdir + '/temp/' + seq_type + '_region_scores/' + sample +'_' + tf + '.txt', sep='\t', index=False)
     return region_score_df
